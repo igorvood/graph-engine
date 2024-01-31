94cbd19a7cff8502e922f847eeebf91175bff9fb
vood <balitsky_igor@list.ru>
2024-01-31 19:52:26 +0300

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,21 +25,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.retry:spring-retry")
-    implementation("org.springframework:spring-aspects")
-    implementation("net.devh:grpc-server-spring-boot-starter:2.14.0.RELEASE")
-    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
     implementation("org.springframework.statemachine:spring-statemachine-starter:3.2.0")
-//    implementation("org.springframework.statemachine:spring-statemachine-core:3.2.0.RELEASE")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
